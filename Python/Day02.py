e98bbd4f51ad6cf4ee66525bc06561917adb9948
Lohith KG-INT <lohith.kg@indnord.com>
2026-08-11 20:54:42 +0530

    Tuesday

---
+++ Python/Day02.py
@@ -0,0 +1,32 @@
+#Monday
+
+#while loops
+i = 0
+
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+
+#For loops
+fruits = ["apple", "banana", "cherry"]
+
+for x in fruits:
+  print(x)
+  if x == "banana":
+    break
+
+#Functions
+def greet(name):
+  print("Hello, " + name)
+
+greet("Emil")
+
+#String 
+txt = "Hello, World!"
+
+print(txt[2:5])
+print(txt.upper())
+name = "Python"
+print(f"I love {name}")
